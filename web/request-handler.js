1d061d91c27dbd39d6835c00d9554d164d675324
egeniesse <egeniesse@gmail.com>
2015-12-16 19:21:08 -0800

    added a callback function that can be used in multiple ending scenario

--- web/request-handler.js
+++ web/request-handler.js
@@ -4,28 +4,49 @@ var url = require('url');
 var fs = require("fs");
 // require more modules/folders here!
 var header = [
-  'Content-Type', 'application/json'
+  "Content-Type", 'application/json'
 ]
 
 
 exports.handleRequest = function (req, res) {
-  res.setHeader("content-type", 'application/json' );
-  res.statusCode = 200;
+  // var header = "content-type", 'application/json';
+  var statusCode = 200;
   var url = req.url
-  // archive.isUrlInList("google.com"), function(result){
-  //   console.log(result)
-  // }
+  // archive.isUrlInList("google.com", function(bool){
+  //   console.log(bool)
+  // });
   if(req.method === "GET"){
 
     if(url === "/"){
-      var indexContent = fs.readFile(path.join(__dirname , "/public/index.html"), "utf-8", function(err, content){
+      fs.readFile(path.join(__dirname , "/public/index.html"), "utf-8", function(err, content){
         if(err){
-          res.statusCode = 404;
+          resEnd(res, null, header, 404);
         }else{
-          res.end(JSON.stringify(content));
+          resEnd(res, content, header, statusCode);
         }
       });
+    }else{
+      archive.isUrlInList(url , function(result){
+        if(result){
+          fs.readFile(exports.paths.archivedSites + url, "utf-8", function(err, content){
+            if(err){
+              callback(err);
+            }else{
+              callback(content)
+            }
+          })
+        }else{
+          archive.addUrlToList(url);
+          //archive it
+        }  
+      })
     }
   }
   // res.end(archive.paths.list);
 };
+
+var resEnd = function(res, content, header, statusCode){
+  res.setHeader("content-type", "application/json")
+  res.statusCode = statusCode
+  res.end(JSON.stringify(content))
+}
